263b75bd59b2d076e0f091e622fb4139fa22143a
kkjy99 <karyl_kang@mymail.sutd.edu.sg>
2025-05-02 15:33:19 +0800

    Added javascript code

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Script */
+// Collapsible elements
+var collapse = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
